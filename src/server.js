503b007bb3a88dae8933402dd41b1b75e63bd5f2
Julia Mendes <juliadepaivams@gmail.com>
2020-08-06 14:54:37 -0300

    Adcionando atualizações

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const proffys = [
     name: "Diego Fernandes", 
     avatar:"https://github.com/diego3g.png",
     whatssap:"88774586",
-    bio:" Entusiasta das melhores tecnologias de química avançada. <br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.", 
+    bio:" Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.", 
     subject: "Química",
     cost: "20",
     weekday:[0],
@@ -14,23 +14,68 @@ const proffys = [
     name: "Daniele Souza", 
     avatar:"https://github.com/diego3g.png",
     whatssap:"88774586",
-    bio:" Entusiasta das melhores tecnologias de química avançada. <br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.", 
+    bio:" Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.", 
     subject: "Química",
     cost: "20",
     weekday:[1],
     time_from:[720],
     time_to: [1220]
+  },
+  {
+    name: "Fernando Monteiro", 
+    avatar:"https://github.com/diego3g.png",
+    whatssap:"88774586",
+    bio:" Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Matemática",
+    cost: "40",
+    weekday:[3],
+    time_from:[420],
+    time_to: [1320]
   }
+
+]
+
+const subjects = [
+"Artes",
+"Biologia",
+"Ciências",
+"Educação física",
+"Física",
+"Geografia",
+"História",
+"Matemática",
+"Portugês",
+"Química",
+
+]
+const weekdays = [
+"Domingo",
+"Segunda-feira",
+"Terça-feira",
+"Quarta-feira",
+"Quinta-feira",
+"Sexta-feira",
+"Sábado",
+
 ]
 
 function pageLanding(req, res) {
   return res.render("index.html")
 }
 function pageStudy(req, res) {
-  return res.render("study.html", { proffys })
+const filters = req.query
+  return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
 function pageGiveClasses(req, res) {
-  return res.render("give-classes.html")
+  const data = req.query
+  const isNotEmpty = Object.keys(data).length > 0
+if (isNotEmpty) {
+  proffys.push(data)
+
+  return res.redirect("/study")
+}
+  
+  return res.render("give-classes.html", {subjects, weekdays })
 }
 
 
