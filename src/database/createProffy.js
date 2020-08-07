019a8f5f6c57ece08255862f28df86f871f5328c
Julia Mendes <juliadepaivams@gmail.com>
2020-08-07 12:56:58 -0300

    Adicionando alterações

--- src/database/createProffy.js
+++ src/database/createProffy.js
@@ -1,3 +1,36 @@
-module.exports = function(db, {proffyValue, classValue, classScheduleValue}) {
-  
+module.exports = async function(db, {proffyValue, classValue, classScheduleValue}) {
+
+  const insertedProffy = await db.run(`
+  INSERT INTO proffys (
+    name,
+    avatar,
+    whatsapp,
+    bio
+ ) VALUES (
+   ${proffyValue.name},
+   ${proffyValue.avatar},
+   ${proffyValue.whatsapp},
+   ${proffyValue.bio}
+
+  );
+`) 
+
+const proffy_id = insertedProffy.lastID
+
+const insertedClass = await db.run(`
+      INSERT INTO classes (
+        subject,
+        cost,
+        proffy_id
+      )VALUES (
+      ${classValue.subject},
+       ${classValue.cost},
+       ${proffy_id}
+    
+      );
+
+`)
+
+const class_id = insertedProffy.lastID 
+
 }
