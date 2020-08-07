d1e4f93f345723053b6178e17352d5425a159b0e
Julia Mendes <juliadepaivams@gmail.com>
2020-08-07 16:44:58 -0300

    Adicionando alterações

---
+++ src/database/fake_data.js
@@ -0,0 +1,36 @@
+const proffys = [
+  {
+    name: "Diego Fernandes", 
+    avatar:"https://github.com/diego3g.png",
+    whatssap:"88774586",
+    bio:" Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Química",
+    cost: "20",
+    weekday:[0],
+    time_from:[720],
+    time_to: [1220]
+  },
+  {
+    name: "Daniele Souza", 
+    avatar:"https://github.com/diego3g.png",
+    whatssap:"88774586",
+    bio:" Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Química",
+    cost: "20",
+    weekday:[1],
+    time_from:[720],
+    time_to: [1220]
+  },
+  {
+    name: "Fernando Monteiro", 
+    avatar:"https://github.com/diego3g.png",
+    whatssap:"88774586",
+    bio:" Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Matemática",
+    cost: "40",
+    weekday:[3],
+    time_from:[420],
+    time_to: [1320]
+  }
+
+]
